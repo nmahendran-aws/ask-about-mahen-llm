809b61ef5f117f7045d577ca4e74d8664bf17fe8
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2025-11-07 18:43:48 -0500

    Add Terraform configuration for AWS provider

---
+++ terraform/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+provider "aws" {
+   assume_role {
+    role_arn     = "arn:aws:iam::471727841202:role/Terraform_Admin_Role"
+    session_name = "TerraformSession"
+ }
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
